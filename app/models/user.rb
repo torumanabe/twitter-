f3c38dbda290808f14fde10036372d3acc047a73
torumanabe <manabe@fminc.xsrv.jp>
2020-07-07 16:47:43 +0900

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :tweets, dependent: :destroy
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true, length: { in: 1..15 }
+
+end
---
+++ vendor/bundle/ruby/2.6.0/gems/mini_racer-0.2.15/lib/mini_racer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MiniRacer
+  VERSION = "0.2.15"
+end
